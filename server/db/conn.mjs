3e7aa752a6806db3d188cf8d9b5d25f7904f6d61
Sarah Luo <sarrstar888@gmail.com>
2024-01-12 10:06:16 -0500

    updated connection string

---
+++ server/db/conn.mjs
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+// const connectionString = process.env.ATLAS_URI || "";
+const connectionString = "mongodb+srv://sarr:hellohello@cluster0.ns32rdv.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+  console.log("Connecting to MongoDB Atlas...");
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
+}
+
+let db = conn.db("sample_training");
+
+export default db;
